de3f20c5975cc8c4715089a9b65f932527816e43
gnenov89 <georgi.nenov89@gmail.com>
2018-01-30 18:51:31 -0500

    markers finished

--- BostonTours/src/components/map.js
+++ BostonTours/src/components/map.js
@@ -96,6 +96,27 @@ export default class MapBoston extends Component{
             </MapView.Callout> */}
          </MapView.Marker>
                       ))}
+
+                      <Polyline
+		coordinates={[
+			{ latitude: 42.355490, longitude: -71.063815 },
+			{ latitude: 37.7896386, longitude: -122.421646 },
+			{ latitude: 37.7665248, longitude: -122.4161628 },
+			{ latitude: 37.7734153, longitude: -122.4577787 },
+			{ latitude: 37.7948605, longitude: -122.4596065 },
+			{ latitude: 37.8025259, longitude: -122.4351431 }
+		]}
+		strokeColor="#000" // fallback for when `strokeColors` is not supported by the map-provider
+		strokeColors={[
+			'#7F0000',
+			'#00000000', // no color, creates a "long" gradient between the previous and next coordinate
+			'#B24112',
+			'#E5845C',
+			'#238C23',
+			'#7F0000'
+		]}
+		strokeWidth={6}
+	/>
       </MapView.Animated>
     );
   }
